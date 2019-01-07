949395863b62dbcb08e61e4be524071ac9b47191
Adam <aervans@arthrex.com>
2019-01-07 10:59:32 -0500

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DraftComponent } from './draft/draft.component';
+
+const routes: Routes = [
+  {
+    path: 'draft',
+    component: DraftComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DraftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
